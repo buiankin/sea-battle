b9cdb6de077dbedfae8fa29e8be765bf3d6c5a1c
Denis Buiankin <buiankin@yandex.ru>
2021-02-24 17:27:10 +0700

    1

--- src/App.tsx
+++ src/App.tsx
@@ -45,11 +45,91 @@ const initializeAssistant = (getState: any) => {
   return createAssistant({ getState });
 };
 
-
 export const App: FC = memo(() => {
   const [appState, dispatch] = useReducer(reducer, { notes: [] });
 
   const [note, setNote] = useState("");
+
+  const [gameOver, setGameOver] = useState(false);
+
+  let initial_myField = [];
+  let myShips = placeVarious();
+
+  let initial_enemyField = [];
+  let initial_enemyShips = placeVarious();
+
+
+  // первоначальное (пустое) состояние поля
+  //for (let i = 0; i < 10; i++) {
+  //  initial_myField.push([]);
+  //  initial_enemyField.push([]);
+  //}
+
+  for (let i = 0; i < 10; i++) {
+    let val_in_line=[];
+    let enemy_val_in_line=[];
+
+    for (let j = 0; j < 10; j++) {
+      val_in_line.push({
+        x: j,
+        y: i,
+        containsShip: false,
+        shot: false,
+        isShipVisible: false,
+        shipId: null,
+      });
+
+      enemy_val_in_line.push({
+        x: j,
+        y: i,
+        containsShip: false,
+        shot: false,
+        isShipVisible: false,
+        shipId: null,
+      });
+    }
+
+    initial_myField.push(val_in_line);
+    initial_enemyField.push(enemy_val_in_line);
+  }
+
+  let my_grid=[];
+  let enemy_grid=[];
+
+    // Заполняем из наших массивов
+    let remaining_hit_points=0;
+    for (let y = 0; y < 10; y++) {
+      let my_line=[];
+      let enemy_line=[];
+      for (let x = 0; x < 10; x++) {
+        // Наши корабли
+        let fieldVal = initial_myField[y][x];
+        let fieldVal_0 = fieldVal.shot ? Constants.GRID_VALUE_WATER_HIT : Constants.GRID_VALUE_WATER;
+        if (fieldVal.containsShip)
+          fieldVal_0 = fieldVal.shot ? Constants.GRID_VALUE_SHIP_HIT : Constants.GRID_VALUE_SHIP;
+        my_line.push(fieldVal_0);
+        // Корабли оппонента
+        fieldVal = initial_enemyField[y][x];
+        fieldVal_0 = fieldVal.shot ? Constants.GRID_VALUE_WATER_HIT : Constants.GRID_VALUE_WATER;
+        if (fieldVal.containsShip) {
+          fieldVal_0 = fieldVal.shot ? Constants.GRID_VALUE_SHIP_HIT : Constants.GRID_VALUE_SHIP;
+          remaining_hit_points++;
+        }
+        enemy_line.push(fieldVal_0);
+      }
+
+      my_grid.push(my_line);
+      enemy_grid.push(enemy_line);
+  
+    }
+
+    let initial_my_board = { grid:my_grid };
+    let initial_opponent_board = { grid: enemy_grid, remaining_hit_points: 0 };
+
+    initial_opponent_board.remaining_hit_points=remaining_hit_points;
+
+  const [my_board, setMyBoard] = useState(initial_my_board);
+  
 
   const assistantStateRef = useRef<AssistantAppState>();
   const assistantRef = useRef<ReturnType<typeof createAssistant>>();
@@ -76,8 +156,96 @@ export const App: FC = memo(() => {
     };
   }, [appState]);
 
+  function _renderResult() {
+
+    //const { game, playerId, winnerId } = this.props;
+
+    let playerId = 1;
+    let winnerId = 2;
+
+    const message = playerId === winnerId ? 'Yo Ho Ho, victory is yours!' : 'You got wrecked, landlubber!';
+    const twitterMessage = playerId === winnerId ? 'Yo Ho Ho, I won a battle at Phoenix Battleship' : 'I got wrecked at Phoenix Battleship';
+
+    //setDocumentTitle(`${message} · #${game.id}`);
+
+    return (
+      <div id="game_result">
+        <header>
+          {/*<Logo/>*/}
+          <h1>Game over</h1>
+          <p>{message}</p>
+          <a
+            href={`https://twitter.com/intent/tweet?url=https://phoenix-battleship.herokuapp.com&button_hashtag=myelixirstatus&text=${twitterMessage}`}
+            className="twitter-hashtag-button"><i className="fa fa-twitter" /> Share result</a>
+        </header>
+        {/*<Link to="/">Back to home</Link>*/}
+      </div>
+    );
+  }
+
+  function _renderOpponentBoard() {
+    //const { dispatch, game, gameChannel, playerId, currentTurn, readyForBattle } = this.props;
+
+    /*
+    if (!readyForBattle) return (
+        <Instructions
+            readyForBattle={readyForBattle}
+            playerIsAttacker={playerId === game.attacker}/>
+    );
+     */
+
+    //const opponentBoard = this.state.opponent_board_0;
+    // а вот через data к ним можно бы обратиться только внутри OpponentBoard
+    const remaining_hit_points = 10;//this.state.opponent_board.remaining_hit_points;
+
+    return (
+      <div id="opponents_board_container">
+        <header>
+          <h2>Shooting grid</h2>
+        </header>
+      </div>
+    );
+  }
+
+
+  function _renderGameContent() {
+    if (gameOver) return _renderResult();
+
+    return (
+      <section id="main_section">
+        {
+          <Header
+          //game={GameShowView}
+          //playerId={playerId}
+          //currentTurn={currentTurn}
+          >
+          </Header>
+        }
+        <section id="boards_container">
+          <div id="my_board_container">
+            <header>
+              <h2>Your ships</h2>
+            </header>
+            <MyBoard
+              //dispatch={dispatch}
+              //gameChannel={gameChannel}
+              //selectedShip={selectedShip}
+              data={my_board}
+            />
+          </div>
+          {
+            _renderOpponentBoard()
+          }
+        </section>
+      </section>
+    );
+
+  }  
+
+
   return (
-    <main className="container">
+    <main id="game_show" className="view-container">
+      {_renderGameContent()}
       <form
         onSubmit={(event) => {
           event.preventDefault();
